9239503c735b167340f401469b01d5279e4e0632
Joshua Swick <joshuaswickirl@gmail.com>
2019-09-13 08:08:28 -0600

    Refactor to use local variables  and simplify logic

--- hamming/hamming.sh
+++ hamming/hamming.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 main () {
+
+    local output status
     
     if (( $# != 2 )) ; then
         output='Usage: hamming.sh <string1> <string2>'
         status=1
     elif (( ${#1} == ${#2} )); then
         output=0
+        status=0
         for (( i=0; i <= ${#1}; i++ )); do
             if [[ ${1:i:1} != ${2:i:1} ]]; then
                 (( output++ ))
@@ -18,7 +21,7 @@ main () {
     elif (( ${#2} == 0 )); then
         output='right strand must not be empty'
         status=1
-    elif (( ${#1} > ${#2} )) || (( ${#1} < ${#2} )); then
+    elif (( ${#1} != ${#2} )); then
         output='left and right strands must be of equal length'
         status=1
     fi
